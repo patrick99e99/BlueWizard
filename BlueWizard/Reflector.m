8b770a5f0f58d09970fa4c61d38c765b786e8766
Patrick Collins <patrick@collinatorstudios.com>
2017-04-02 10:47:01 -0700

    rms should be limited when not a stop frame

--- BlueWizard/Reflector.m
+++ BlueWizard/Reflector.m
@@ -74,6 +74,14 @@ static NSUInteger const kNumberOfKParameters = 11;
     return (double *)_ks;
 }
 
+-(double)rms {
+    if (self.shouldLimitRMS && _rms >= [CodingTable rms][kStopFrameIndex - 1]) {
+        return [CodingTable rms][kStopFrameIndex - 1];
+    } else {
+        return _rms;
+    }
+}
+
 -(BOOL)isVoiced {
     return ![self isUnvoiced];
 }
