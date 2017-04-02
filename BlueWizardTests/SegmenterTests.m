069806734f37a225fd77512e033398922af4beea
Patrick Collins <patrick@collinatorstudios.com>
2017-04-02 10:53:14 -0700

    fix crash in test

--- BlueWizardTests/SegmenterTests.m
+++ BlueWizardTests/SegmenterTests.m
@@ -11,24 +11,25 @@
 @implementation SegmenterTests {
     Segmenter *subject;
     Buffer *mainBuffer;
+    double *samples;
 }
 
 -(void)setUp {
     [super setUp];
     NSUInteger size = 30;
-    double *samples = malloc(sizeof(float) * size);
+    samples = malloc(sizeof(float) * size);
     for (int i = 0; i < size; i++) {
         samples[i] = 1.0f;
     }
     
     mainBuffer = [[Buffer alloc] initWithSamples:samples size:size sampleRate:8000];
-    free(samples);
     [[UserSettings sharedInstance] setFrameRate:@1];
     subject = [[Segmenter alloc] initWithBuffer:mainBuffer windowWidth:1];
 }
 
 -(void)tearDown {
     [super tearDown];
+    free(samples);
 }
 
 -(void)testItYieldsBuffers {
